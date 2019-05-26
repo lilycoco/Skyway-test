84ddab399f6e81b55fdd7f3290d6c1aff9ba181f
lilycoco <ry.sep27@gmail.com>
2019-05-27 04:05:03 +0900

    modified keys pass

--- skyway-js-sdk/examples/room/script.js
+++ skyway-js-sdk/examples/room/script.js
@@ -23,7 +23,7 @@ const Peer = window.Peer;
   await localVideo.play().catch(console.error);
 
   const peer = new Peer({
-    key: 'b74a568e-c941-4cfe-bf70-67c512a629ac',
+    key: window.__SKYWAY_KEY__,
     debug: 3,
   });
 
